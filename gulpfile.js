f581ff22f907b7f6ad3d5e70beced08c54891d99
Jonas Nilsson <Jonas.Nilsson@adresseavisen.no>
2017-08-04 14:56:59 +0200

    Monsterpush. Agne fix plz.

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ let options = {
     // HTML minify
     enable_htmlmin : true,
     htmlmin : {
-        collapseWhitespace: true
+    	collapseWhitespace: true
     },
 
     // Javascript
@@ -38,41 +38,41 @@ let options = {
     js_concat_some : ['js/filename1.js', 'js/filename2.js', 'js/etc.js'],
 
     images : {
-        '**/*.{png,gif,jpg}': [
-            {
-                width: 1920,
-                rename :  { suffix: '-1920' },
-            }, {
-                width: 1800,
-                rename :  { suffix: '-1800' },
-            }, {
-                width: 1680,
-                rename :  { suffix: '-1680' },
-            }, {
-                width: 1440,
-                rename :  { suffix: '-1440' },
-            }, {
-                width: 1280,
-                rename :  { suffix: '-1280' },
-            }, {
-                width: 1024,
-                rename :  { suffix: '-1024' },
-            }, {
-                width: 800,
-                rename :  { suffix: '-800' },
-            }, {
-                width: 640,
-                rename :  { suffix: '-640' },
-            }, {
-                width: 480,
-                rename :  { suffix: '-480' },
-            }, {
-                width: 320,
-                rename :  { suffix: '-320' },
-            }, {
-                width: 160,
-            }
-        ]
+    	'**/*.{png,gif,jpg}': [
+    	{
+    		width: 1920,
+    		rename :  { suffix: '-1920' },
+    	}, {
+    		width: 1800,
+    		rename :  { suffix: '-1800' },
+    	}, {
+    		width: 1680,
+    		rename :  { suffix: '-1680' },
+    	}, {
+    		width: 1440,
+    		rename :  { suffix: '-1440' },
+    	}, {
+    		width: 1280,
+    		rename :  { suffix: '-1280' },
+    	}, {
+    		width: 1024,
+    		rename :  { suffix: '-1024' },
+    	}, {
+    		width: 800,
+    		rename :  { suffix: '-800' },
+    	}, {
+    		width: 640,
+    		rename :  { suffix: '-640' },
+    	}, {
+    		width: 480,
+    		rename :  { suffix: '-480' },
+    	}, {
+    		width: 320,
+    		rename :  { suffix: '-320' },
+    	}, {
+    		width: 160,
+    	}
+    	]
     },
 };
 
@@ -120,12 +120,14 @@ const runSequence       = require('run-sequence');
 
 // Paths
 let paths = {
-    scss: 'src/scss/**/*',
-    js: 'src/js/**/*',
-    html: 'src/html/**/*',
-    scale: 'src/scale/images/**/*',
-    fonts: 'src/fonts/**/*',
-    images: options.source_folder+'/images/'
+	scss: 'src/scss/**/*',
+	js: 'src/js/**/*',
+	html: 'src/html/**/*',
+	scale: 'src/scale/images/**/*',
+	fonts: 'src/fonts/**/*',
+	gfx: 'src/gfx/**/*',
+	fodata: 'src/data/**/*',
+	images: options.source_folder+'/images/'
 };
 
 //--------------------------------------------------//
@@ -134,7 +136,7 @@ let paths = {
 
 // Scss / css
 gulp.task('scss', function() {
-    return gulp.src(paths.scss+'.scss')
+	return gulp.src(paths.scss+'.scss')
             //Compile Scss
             .pipe(sass().on('error', sass.logError))
 
@@ -143,10 +145,10 @@ gulp.task('scss', function() {
 
             // Add autoprefixer
             .pipe(postcss([
-                autoprefixer({
-                    browsers: ['last '+options.autoprefixer+' versions']
-                }),
-            ]))
+            	autoprefixer({
+            		browsers: ['last '+options.autoprefixer+' versions']
+            	}),
+            	]))
 
             // Write sourcemaps
             .pipe(gulpif(options.sourcemaps, sourcemaps.write()))
@@ -156,33 +158,33 @@ gulp.task('scss', function() {
 
             // Deliver to src/css
             .pipe(gulp.dest(options.source_folder+'/css/')).on('end', function() {
-                gutil.log('Source compiled');
+            	gutil.log('Source compiled');
             })
-});
+        });
 
 
 gulp.task('js', function() {
-    return gulp.src(gulpif(options.js_concat_all, paths.js+'.js', options.js_concat_some))
-        .pipe(concat(options.js_concat_name))
-        .pipe(gulp.dest(options.source_folder+'/js/'));
+	return gulp.src(gulpif(options.js_concat_all, paths.js+'.js', options.js_concat_some))
+	.pipe(concat(options.js_concat_name))
+	.pipe(gulp.dest(options.source_folder+'/js/'));
 });
 
 gulp.task('html', function(){
-    return gulp.src('src/**/*.html')
-        .pipe(gulp.dest(options.source_folder+'/'));
+	return gulp.src('src/**/*.html')
+	.pipe(gulp.dest(options.source_folder+'/'));
 });
 
 // Churn deeze images
 gulp.task('rs-img', function () {
-    var dest = options.source_folder+'/images';
-    return gulp.src(paths.scale)
+	var dest = options.source_folder+'/images';
+	return gulp.src(paths.scale)
         // cachename can be anything, does not need to be rs-img
         .pipe(changed(paths.images))
         .pipe(responsive(options.images).on('error', function() {
-            gutil.log('Responsive image error');
+        	gutil.log('Responsive image error');
         }))
         .pipe(gulp.dest(paths.images))
-});
+    });
 
 
 //--------------------------------------------------//
@@ -191,22 +193,22 @@ gulp.task('rs-img', function () {
 
 // Compress images from prod to dist
 gulp.task('dist:compress', function() {
-    return gulp.src(paths.images+'**/*')
-        .pipe(changed(options.destination_folder+'/images'))
-        .pipe(imagemin())
-        .pipe(gulp.dest(options.destination_folder+'/images'))
+	return gulp.src(paths.images+'**/*')
+	.pipe(changed(options.destination_folder+'/images'))
+	.pipe(imagemin())
+	.pipe(gulp.dest(options.destination_folder+'/images'))
 });
 
 // Build distribution javascript
 gulp.task('dist:js', function() {
-    return gulp.src(options.source_folder+'/js/'+options.js_concat_name)
-        .pipe(uglify())
-        .pipe(gulp.dest(options.destination_folder+'/js/'));
+	return gulp.src(options.source_folder+'/js/'+options.js_concat_name)
+	.pipe(uglify())
+	.pipe(gulp.dest(options.destination_folder+'/js/'));
 });
 
 // Build distribution css
 gulp.task('dist:css', function() {
-    return gulp.src(options.source_folder+'/css/**/*.css')
+	return gulp.src(options.source_folder+'/css/**/*.css')
         // Css concatination
         .pipe(concat(options.main_css_file))
 
@@ -215,15 +217,15 @@ gulp.task('dist:css', function() {
 
         // Deliver to dist/css
         .pipe(gulp.dest(options.destination_folder+'/css/')).on('end', function() {
-            gutil.log('Source -> Dist Concat');
+        	gutil.log('Source -> Dist Concat');
         })
-});
+    });
 
 // Build distribution html file
 gulp.task('dist:html', function(){
-    return gulp.src(options.source_folder+'/**/*.html')
-        .pipe(gulpif(options.enable_htmlmin, htmlmin(options.htmlmin)))
-        .pipe(gulp.dest(options.destination_folder+'/'));
+	return gulp.src(options.source_folder+'/**/*.html')
+	.pipe(gulpif(options.enable_htmlmin, htmlmin(options.htmlmin)))
+	.pipe(gulp.dest(options.destination_folder+'/'));
 });
 
 //--------------------------------------------------//
@@ -232,17 +234,14 @@ gulp.task('dist:html', function(){
 
 // Watch deeze folders
 gulp.task('watch', function() {
-    gulp.watch(paths.scss, ['scss']);
-    gulp.watch(paths.js, ['js']);
-    gulp.watch(paths.scale, ['rs-img']);
-    gulp.watch('src/**/*.html', ['html']);
+	gulp.watch(paths.scss, ['scss']);
+	gulp.watch(paths.js, ['js']);
+	gulp.watch(paths.scale, ['rs-img']);
+	gulp.watch('src/**/*.html', ['html']);
 
-    gulp.src('src/data/**/*')
-        .pipe(gulp.dest('prod/data'));
-
-    gulp.src(paths.fonts)
-        .pipe(changed(paths.fonts))
-        .pipe(gulp.dest('prod/fonts'));
+	gulp.src([paths.fonts, paths.gfx, paths.data])
+	.pipe(changed('prod'))
+	.pipe(gulp.dest('prod'));
 
 });
 
@@ -251,7 +250,7 @@ gulp.task('img', ['rs-img']);
 
 // Clear production and distribution folder
 gulp.task('build:clear', function() {
-    return gulpif(options.source_folder !== 'src', del([options.source_folder+'**/*', options.source_folder, options.destination_folder+'**/*', options.destination_folder]));
+	return gulpif(options.source_folder !== 'src', del([options.source_folder+'**/*', options.source_folder, options.destination_folder+'**/*', options.destination_folder]));
 });
 
 
@@ -259,9 +258,9 @@ gulp.task('build:source', ['scss', 'js', 'html', 'rs-img']);
 
 // Distribution task
 gulp.task('build:dist', function() {
-    runSequence('build:source', 'dist:css', 'dist:html', 'dist:js', 'dist:compress', function() {
-        gutil.log(chalk.bold.green('Your Project is now ready for the web!'));
-    });
+	runSequence('build:source', 'dist:css', 'dist:html', 'dist:js', 'dist:compress', function() {
+		gutil.log(chalk.bold.green('Your Project is now ready for the web!'));
+	});
 });
 
 // Build all
@@ -273,74 +272,74 @@ gulp.task('default', ['watch', 'build:source']);
 const green = chalk.green;
 const bold = chalk.bold.green;
 gulp.task('help', function () {
-    gutil.log(`
+	gutil.log(`
 
 
-                                                            _
-             /\\                                            | |
-            /  \\    __ _  _ __    ___    ___    __ _       | |  ___   _ __    __ _  ___
-           / /\\ \\  / _' || '_ \\  / _ \\  / _ \\  / _' |  _   | | / _ \\ | '_ \\  / _' |/ __|
-          / ____ \\| (_| || | | ||  __/ | (_) || (_| | | |__| || (_) || | | || (_| |\\__ \\
-         /_/    \\_\\\\__, ||_| |_| \\___|  \\___/  \\__, |  \\____/  \\___/ |_| |_| \\__,_||___/
-                    __/ |                       __/ |
-                   |___/                       |___/
+		_
+		/\\                                            | |
+		/  \\    __ _  _ __    ___    ___    __ _       | |  ___   _ __    __ _  ___
+		/ /\\ \\  / _' || '_ \\  / _ \\  / _ \\  / _' |  _   | | / _ \\ | '_ \\  / _' |/ __|
+		/ ____ \\| (_| || | | ||  __/ | (_) || (_| | | |__| || (_) || | | || (_| |\\__ \\
+		/_/    \\_\\\\__, ||_| |_| \\___|  \\___/  \\__, |  \\____/  \\___/ |_| |_| \\__,_||___/
+		__/ |                       __/ |
+		|___/                       |___/
 
 
-    ${green('$ gulp')}
+		${green('$ gulp')}
 
-    ${green('$ gulp help')}
-        this message
+		${green('$ gulp help')}
+		this message
 
-    ${bold('build: [source|dist|clear]')}
+		${bold('build: [source|dist|clear]')}
 
-    ${green('$ gulp build')}
-        Build Source and Dist
+		${green('$ gulp build')}
+		Build Source and Dist
 
-    ${green('$ gulp build:source')}
-        Build a production enviorment for the project.
-        Will not compress any files.
-        adds sourcemaps
+		${green('$ gulp build:source')}
+		Build a production enviorment for the project.
+		Will not compress any files.
+		adds sourcemaps
 
-    ${green('$ gulp build:dist')}
-        Build a distribution enviorment for the project.
-        will compress all files, no sourcemaps
+		${green('$ gulp build:dist')}
+		Build a distribution enviorment for the project.
+		will compress all files, no sourcemaps
 
-    ${green('$ gulp build:clear')}
-        will delete both distribution and production enviorments.
+		${green('$ gulp build:clear')}
+		will delete both distribution and production enviorments.
 
-    ${bold('Single actions Production:')}
+		${bold('Single actions Production:')}
 
-    ${green('$ gulp scss')}
-        Compile and concat scss
+		${green('$ gulp scss')}
+		Compile and concat scss
 
-    ${green('$ gulp js')}
-        Compile and concat js to `+options.source_folder+`/js/`+options.js_concat_name+`
+		${green('$ gulp js')}
+		Compile and concat js to `+options.source_folder+`/js/`+options.js_concat_name+`
 
-    ${green('$ gulp html')}
-        Send src/*.html to `+options.source_folder+`/*.html
+		${green('$ gulp html')}
+		Send src/*.html to `+options.source_folder+`/*.html
 
-    ${green('$ gulp rs-img')}
-        will compile compressed versions of all files under scale/images/**
-        works with jpg,png and gif
+		${green('$ gulp rs-img')}
+		will compile compressed versions of all files under scale/images/**
+		works with jpg,png and gif
 
-    ${green('$ gulp img')}
-        Alias of $ gulp rs-img
+		${green('$ gulp img')}
+		Alias of $ gulp rs-img
 
-    ${bold('Single actions Distribution:')}
+		${bold('Single actions Distribution:')}
 
-    ${green('$ gulp dist:compress')}
-        Compress images from Production to `+options.destination_folder+`/
+		${green('$ gulp dist:compress')}
+		Compress images from Production to `+options.destination_folder+`/
 
-    ${green('$ gulp dist:html')}
-        Compress html to `+options.destination_folder+`/
+		${green('$ gulp dist:html')}
+		Compress html to `+options.destination_folder+`/
 
-    ${green('$ gulp dist:css')}
-        Compress scss/css to `+options.destination_folder+`/css/`+options.main_css_file+`
+		${green('$ gulp dist:css')}
+		Compress scss/css to `+options.destination_folder+`/css/`+options.main_css_file+`
 
-    ${green('$ gulp dist:js')}
-        Compress and concat js to `+options.destination_folder+`/js/`+options.js_concat_name+`
+		${green('$ gulp dist:js')}
+		Compress and concat js to `+options.destination_folder+`/js/`+options.js_concat_name+`
 
 
-    `);
+		`);
 
 });
